c5f9bd1267648bbff515cc27da64075a6eda18fd
Tufail <siddikitufailahmed@gmail.com>
2022-07-25 17:54:08 +0530

    method execution example

--- MethodExecution.java
+++ MethodExecution.java
@@ -1,10 +1,15 @@
 package mypack0;
 
 public class MethodExecution {
-
+	
+	public int multiply(int a,int b) {
+		return a*b; 
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		MethodExecution obj = new MethodExecution();
+		System.out.println("Multiplication of two number is:- "+obj.multiply(20, 20));
+		
 	}
 
 }
