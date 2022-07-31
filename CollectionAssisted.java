c73564c78ff87e0fe58524f293e0515493292823
Tufail <siddikitufailahmed@gmail.com>
2022-07-31 23:16:39 +0530

    ArrayList Example

---
+++ CollectionAssisted.java
@@ -0,0 +1,18 @@
+package mypack0;
+import java.util.*;
+public class CollectionAssisted {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//creating arraylist
+		System.out.println("ArrayList");
+		ArrayList<String> city = new ArrayList<String>();
+		city.add("Mumbai");
+		city.add("Dubai");
+		city.add("Chennai");
+		city.add("Delhi");
+		System.out.println(city);
+
+	}
+
+}
